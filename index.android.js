9e57b87a85d31f49b52bd3ac5cecf94aac0261d7
Fransiskus Setiawan <fgn.dwikasetiawan@gmail.com>
2017-05-30 20:58:24 +0700

    home screen implemented

--- index.android.js
+++ index.android.js
@@ -5,16 +5,39 @@ import AddItemScreen from './add-item-screen.js';
 import StockReportScreen from './stock-report-screen.js';
 import AddTransactionScreen from './add-transaction-screen.js';
 import TransactionReportScreen from './transaction-report-screen.js';
-import { AppRegistry, View, Text, TouchableOpacity, StyleSheet, ActivityIndicator, ToastAndroid} from 'react-native';
+import { AppRegistry, View, Text, TouchableOpacity, StyleSheet, ActivityIndicator, ToastAndroid, TouchableNativeFeedback} from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
 import { StackNavigator, DrawerNavigator } from 'react-navigation';
 import Realm from 'realm';
 import { Barang, Stok, Transaksi, BarangTransaksi, RealmConfigs } from './database.js';
 import {Button} from './buttons.js';
 
+function MenuCard(props) {
+	return(
+		<TouchableOpacity style={[{
+			backgroundColor: 'white',
+			elevation: 2
+		}, props.cardStyle]}
+			onPress={props.onPress}
+		>
+			<View
+				style={[{paddingTop: 10, paddingBottom: 10, height: props.iconHeight || 100, justifyContent: 'center', alignItems: 'center'}, props.iconStyle]}
+			>
+				{props.icon}
+			</View>
+			<View style={[{paddingBottom: 20, justifyContent: 'center', alignItems: 'center', backgroundColor: props.titleBackgroundColor || 'white'}, props.titleBackgroundStyle]}>
+				<Text style={{fontSize: 16, color: props.titleColor || 'black', fontWeight: 'bold'}}>{props.title}</Text>
+			</View>
+		</TouchableOpacity>
+	);
+}
+
 class Main extends React.Component {
 
 	static navigationOptions = {
-		headerVisible: false
+		headerTitle: 'Beranda',
+		headerTitleStyle: {color: 'white', fontWeight: 'bold'},
+		headerStyle: {backgroundColor: '#5af'}
 	};
 
 	constructor(props) {
@@ -27,7 +50,6 @@ class Main extends React.Component {
 			}),
 			isReady: true
 		};
-
 
 		// //Make a realm object synced with the server
 		// Realm.Sync.User.login(RealmConfigs.SERVER_ADDRESS, 'maria.setiawati0801@gmail.com', 'palasari',
@@ -72,26 +94,54 @@ class Main extends React.Component {
 		}
 
 		return(
-			<View>
-				<View style={{paddingTop: 10}}>
-					<Text>Main screen</Text>
+			<View style={{paddingLeft: 10, paddingRight: 10, paddingTop: 10}}>
+				
+				<View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
+					<MenuCard 
+						cardStyle={{flex: 0.475}}
+						icon={<Icon name="list" style={{color: '#0af', fontSize: 64}}/>}
+						titleColor="grey"
+						title="Daftar Barang"
+						onPress={() => navigate('ItemSearch', {realm: this.state.realm})}
+					/>
+					<View style={{flex: 0.05}} />
+					<MenuCard 
+						cardStyle={{flex: 0.475}}
+						icon={<Icon name="plus" style={{color: 'limegreen', fontSize: 64}}/>}
+						titleColor="grey"
+						title="Barang Baru"
+						onPress={() => navigate('AddItem', {realm: this.state.realm})}
+					/>
 				</View>
-				<View style={{flexDirection: 'column'}}>
-					<TouchableOpacity 
-					 style={style.button}
-					 onPress={() => navigate('ItemSearch', {realm: this.state.realm})} >
-					 	<Text style={style.buttonText}>CARI BARANG</Text>
-					</TouchableOpacity>
 
-					<TouchableOpacity 
-					style={style.button}
-					onPress={() => navigate('AddItem', {realm: this.state.realm})} >
-					<Text style={style.buttonText}>TAMBAH BARANG</Text>
-					</TouchableOpacity>
-					
-					<Button style={{width: 180}} title="STOK DAN KADALUARSA" onPress={() => navigate('StockReport', {realm: this.state.realm})} />
-					<Button title="TRANSAKSI BARU" onPress={() => navigate('AddTransaction', {realm: this.state.realm})} />
-					<Button title="LAPORAN TRANSAKSI" onPress={() => navigate('TransactionReport', {realm: this.state.realm})} />
+				<View style={{marginTop: 30, flexDirection: 'row', justifyContent: 'center'}}>
+					<View style={{flex: 0.225}}/>
+					<MenuCard 
+						cardStyle={{flex: 0.5}}
+						icon={<Icon name="cubes" style={{color: '#800', fontSize: 64}}/>}
+						titleColor="grey"
+						title="Stok dan Kadaluarsa"
+						onPress={() => navigate('StockReport', {realm: this.state.realm})}
+					/>
+					<View style={{flex: 0.225}} />
+				</View>
+
+				<View style={{marginTop: 30, flexDirection: 'row', justifyContent: 'space-between'}}>
+					<MenuCard 
+						cardStyle={{flex: 0.475}}
+						icon={<Icon name="cart-plus" style={{color: '#f92', fontSize: 64}}/>}
+						titleColor="grey"
+						title="Transaksi Baru"
+						onPress={() => navigate('AddTransaction', {realm: this.state.realm})}
+					/>
+					<View style={{flex: 0.05}} />
+					<MenuCard 
+						cardStyle={{flex: 0.475}}
+						icon={<Icon name="list-alt" style={{color: '#f49', fontSize: 64}}/>}
+						titleColor="grey"
+						title="Laporan Transaksi"
+						onPress={() => navigate('TransactionReport', {realm: this.state.realm})}
+					/>
 				</View>
 			</View>
 		);
